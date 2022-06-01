e9cc7b4c93400887289b6a4f70c68450fdb64ea3
Barbara Amoros <amoros@google.com>
2022-06-01 18:02:44 +0000

    remove language switch which was causing errors

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -37,23 +37,23 @@ export class AppComponent {
 
   onReset() {
     this.eventService.resetInterface.emit();
-    this.languageSwitch('en-US', null);
+    // this.languageSwitch('en-US', null);
   }
 
 
-  languageSwitch(lang: string, e: Event) {
-    let flags = document.getElementsByClassName('flag');
-    for (let i = 0; i < flags.length; i++) {
-      flags[i].className = 'flag inactive';
-    }
-    if(e === null) {
-      flags[0].className = 'flag active';
-    } else {
-      let element = e.target as HTMLElement;
-      element.className = 'flag active';
-    }
-    this.ioService.setDefaultLanguage(lang);
-  }
+  // languageSwitch(lang: string, e: Event) {
+  //   let flags = document.getElementsByClassName('flag');
+  //   for (let i = 0; i < flags.length; i++) {
+  //     flags[i].className = 'flag inactive';
+  //   }
+  //   if(e === null) {
+  //     flags[0].className = 'flag active';
+  //   } else {
+  //     let element = e.target as HTMLElement;
+  //     element.className = 'flag active';
+  //   }
+  //   this.ioService.setDefaultLanguage(lang);
+  // }
 
   /**
    * Chrome on iOS (iPhone & iPad) can't make use of WebRTC & getUserMedia()
